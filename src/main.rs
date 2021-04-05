55ce548f9786ebdccd10edcd77caffb29a1ca69e
Kot <kot@yukata.tech>
2021-04-04 18:26:06 -0700

    Use game_loop to separate update/render

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@
 
 mod assets;
 
+use std::sync::Mutex;
+
 use assets::TypefaceContainer;
-use skulpin::skia_safe::{self, Font};
+use game_loop::game_loop;
 use skulpin::CoordinateSystemHelper;
 use skulpin::{rafx::api::RafxExtents2D, skia_bindings::SkTextUtils_Align};
+use skulpin::{
+    skia_safe::{self, Font},
+    Renderer,
+};
+use winit::{event::Event, window::Window};
 
 #[cfg(debug_assertions)]
 fn debug_builtin_cache() {
@@ -59,7 +66,7 @@ fn main() {
         return;
     }
 
-    let mut renderer = renderer.unwrap();
+    let renderer = renderer.unwrap();
 
     // Setup the cache
     if let Some(user) = assets::user() {
@@ -69,81 +76,118 @@ fn main() {
     // Enable hot reloading for dynamic builtin cache (if present)
     debug_builtin_cache();
 
-    // Increment a frame count so we can render something that moves
-    let mut frame_count: u64 = 0;
+    let lily_app = LilyApp::new(renderer);
 
-    // Start the window event loop. Winit will not return once run is called. We will get notified
-    // when important events happen.
-    event_loop.run(move |event, _window_target, control_flow| {
-        match event {
-            winit::event::Event::WindowEvent {
-                event: winit::event::WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = winit::event_loop::ControlFlow::Exit,
-
-            // Request a redraw any time we finish processing events
-            winit::event::Event::MainEventsCleared => {
-                window.request_redraw();
+    game_loop(
+        event_loop,
+        window,
+        lily_app,
+        240,
+        0.1,
+        |g| g.game.update(),
+        |g| g.game.render(&g.window),
+        |g, event| {
+            if !g.game.handle_event(event) {
+                g.exit();
             }
-
-            winit::event::Event::RedrawRequested(_window_id) => {
-                let window_size = window.inner_size();
-                let window_extents = RafxExtents2D {
-                    width: window_size.width,
-                    height: window_size.height,
-                };
-
-                if let Err(e) = renderer.draw(
-                    window_extents,
-                    window.scale_factor(),
-                    |canvas, coordinate_system_helper| {
-                        draw(canvas, coordinate_system_helper, frame_count);
-                        frame_count += 1;
-                    },
-                ) {
-                    println!("Error during draw: {:?}", e);
-                    *control_flow = winit::event_loop::ControlFlow::Exit
-                }
-            }
-            _ => {}
-        }
-    });
+        },
+    );
 }
 
-fn draw(
-    canvas: &mut skia_safe::Canvas,
-    _coordinate_system_helper: CoordinateSystemHelper,
-    frame_count: u64,
-) {
-    canvas.clear(skia_safe::Color::from_argb(0, 0, 0, 255));
+struct LilyApp {
+    tick_count: u64,
+    renderer: Mutex<Renderer>,
+}
 
-    // Floating point value constantly moving between 0..1 to generate some movement
-    let f = ((frame_count as f32 / 1000.0).sin() + 1.0) / 2.0;
+impl LilyApp {
+    pub fn new(renderer: Renderer) -> Self {
+        Self {
+            tick_count: 0,
+            renderer: Mutex::new(renderer),
+        }
+    }
 
-    // Make a color to draw with
-    let mut paint = skia_safe::Paint::new(skia_safe::Color4f::new(f, 0.0, 1.0 - f, 1.0), None);
-    paint.set_anti_alias(false);
-    paint.set_style(skia_safe::paint::Style::Stroke);
-    paint.set_stroke_width(2.0);
+    pub fn update(self: &mut Self) {
+        self.tick_count += 1;
+    }
 
-    // Draw a rectangle
-    canvas.draw_rect(
-        skia_safe::Rect {
-            left: 10.0,
-            top: 10.0,
-            right: 890.0,
-            bottom: 590.0,
-        },
-        &paint,
-    );
+    pub fn handle_event(self: &Self, event: Event<()>) -> bool {
+        match event {
+            winit::event::Event::WindowEvent {
+                event:
+                    winit::event::WindowEvent::CloseRequested
+                    | winit::event::WindowEvent::KeyboardInput {
+                        input:
+                            winit::event::KeyboardInput {
+                                virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    },
+                ..
+            } => return false,
+            _ => {}
+        }
 
-    let handle = assets::builtin()
-        .load::<TypefaceContainer>("PetMe64")
-        .unwrap();
+        true
+    }
 
-    let typeface = &handle.read().0;
+    pub fn render(self: &Self, window: &Window) {
+        let window_size = window.inner_size();
+        let window_extents = RafxExtents2D {
+            width: window_size.width,
+            height: window_size.height,
+        };
 
-    let font = Font::from_typeface(typeface, 50.0);
+        let mut renderer = self.renderer.lock().unwrap();
 
-    canvas.draw_str_align("owo", (450, 300), &font, &paint, SkTextUtils_Align::Center);
+        if let Err(e) = renderer.draw(
+            window_extents,
+            window.scale_factor(),
+            |canvas, coordinate_system_helper| {
+                self.draw(canvas, coordinate_system_helper, self.tick_count);
+            },
+        ) {
+            println!("Error during draw: {:?}", e);
+        }
+    }
+
+    fn draw(
+        self: &Self,
+        canvas: &mut skia_safe::Canvas,
+        _coordinate_system_helper: CoordinateSystemHelper,
+        tick_count: u64,
+    ) {
+        canvas.clear(skia_safe::Color::from_argb(0, 0, 0, 255));
+
+        // Floating point value constantly moving between 0..1 to generate some movement
+        let f = ((tick_count as f32 / 500.0).sin() + 1.0) / 2.0;
+
+        // Make a color to draw with
+        let mut paint = skia_safe::Paint::new(skia_safe::Color4f::new(f, 0.0, 1.0 - f, 1.0), None);
+        paint.set_anti_alias(false);
+        paint.set_style(skia_safe::paint::Style::Stroke);
+        paint.set_stroke_width(2.0);
+
+        // Draw a rectangle
+        canvas.draw_rect(
+            skia_safe::Rect {
+                left: 10.0,
+                top: 10.0,
+                right: 890.0,
+                bottom: 590.0,
+            },
+            &paint,
+        );
+
+        let handle = assets::builtin()
+            .load::<TypefaceContainer>("PetMe64")
+            .unwrap();
+
+        let typeface = &handle.read().0;
+
+        let font = Font::from_typeface(typeface, 50.0);
+
+        canvas.draw_str_align("hi", (450, 300), &font, &paint, SkTextUtils_Align::Center);
+    }
 }
